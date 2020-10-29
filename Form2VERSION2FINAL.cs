c625a76a03e191c1e10ed2999ed181e97f2b10d0
WILYM <73612644+WILYM@users.noreply.github.com>
2020-10-28 19:40:16 -0500

    Update Form2VERSION2FINAL.cs

--- Form2VERSION2FINAL.cs
+++ Form2VERSION2FINAL.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace METODOLOGIAS
 {
@@ -29,19 +30,73 @@ namespace METODOLOGIAS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int A = int.Parse(TEXTN1.Text);
-            if (A >10000 || A< 0)
+            string[] A = new string[] { "CPU", "MONITOR", "TECLADO", "MOUSE",
+            "ESCRIT. MELAMINA","SILLA GIRATORIA","BANQUITO DE MADERA","EXTINTOR"};
+            string[] B = new string[] { "BUENO", "MALO", "REGULAR", "NO FUNCIONA" };
+            string[] C = new string[] { "HETDO98766", "HYWSQ75308", "HSOHAL8990", "HGET77288" };
+            string[] D = new string[] { "1 AÑO", "2 AÑOS", "6 MESES", "1 AÑO Y 2 MESES", "10 MESES", "1 AÑO Y MEDIO" };
+            string[] E = new string[] { "CODIGO DE PATRIMONIO", "USUARIO" };
+            //int B = int.Parse(codig.Text);
+            Random rnd = new Random();
+            int ra = rnd.Next(0, 2);
+            if (ra == 1)
             {
-                report.Text = "USUARIO NO EXISTE....";
+                validar.Text = "PATRIMONIO Y USUARIO EXISTE ";
+                //int A = int.Parse(TEXTN1.Text);
+                report.Text = "COD.PATRIMONIO =" + codig.Text;
+                report1.Text = "USUARIO =" + TEXTN1.Text;
+                report2.Text = "DESCRIPCION = " + A[rnd.Next(0, 8)];
+                report3.Text = "ESTADO = " + B[rnd.Next(0, 4)];
+                report4.Text = "MODELO = " + C[rnd.Next(0, 4)];
+                report5.Text = "AÑOS DE USO = " + D[rnd.Next(0, 6)];
             }
             else
-                report.Text = "REPORTE GENERADO....!!!!";
+            {
+                int p = rnd.Next(0, 1);
+                validar.Text = E[p] + " NO EXISTE.... VERIFIQUE NUEVAMENTE";
+                report.Text = "VACIO";
+                report1.Text = "VACIO";
+                report2.Text = "VACIO";
+                report3.Text = "VACIO";
+                report4.Text = "VACIO";
+                report5.Text = "VACIO ";
+            }
+            /*if (A > 10000 || A < 0)
+            {
+                report.Text = "EN PROCESO....";
+            }
+            else
+                report.Text = "EN PROCESO....!!!!";*/
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             TEXTN1.Text = "";
+            codig.Text = "";
             report.Text = "";
+            report1.Text = "";
+            report2.Text = "";
+            report3.Text = "";
+            report4.Text = "";
+            report5.Text = "";
+            validar.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void TEXTN1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void report5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
